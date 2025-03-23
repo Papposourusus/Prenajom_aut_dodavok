8dfb19ff4b18176bd019eb5a42e9786ec68b81ad
Papposourusus <masikpatrik@gmail.com>
2025-03-23 13:14:31 +0100

    footer_header_nav(1)

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="author" content="templatemo">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+  <title>SnapX Photography by TemplateMo</title>
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/templatemo-snapx-photography.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+  <link rel="stylesheet" href="assets/css/animate.css">
+  <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css"/>
+</head>
+<body>
+  <?php include 'header.php'; ?>
+  <?php include 'nav.php'; ?>
+  <!-- Vaše hlavné obsahové časti -->
+  <div class="main-banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 offset-lg-1">
+          <div class="header-text">
+            <h2>Enter a world of <em>Photos</em> & Amazing <em>Awards</em></h2>
+            <p>SnapX Photography is a professional website template with 5 different HTML pages for maximum customizations. It is free for commercial usage. This Bootstrap v5.1.3 CSS layout is provided by TemplateMo Free CSS Templates.</p>
+            <div class="buttons">
+              <div class="big-border-button">
+                <a href="contests.php">Explore SnapX Contest</a>
+              </div>
+              <div class="icon-button">
+                <a href="https://youtube.com/templatemo" target="_blank"><i class="fa fa-play"></i> Watch Our Video Now</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <?php include 'footer.php'; ?>
+  <!-- Scripts -->
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/js/isotope.min.js"></script>
+  <script src="assets/js/owl-carousel.js"></script>
+  <script src="assets/js/tabs.js"></script>
+  <script src="assets/js/popup.js"></script>
+  <script src="assets/js/custom.js"></script>
+</body>
+</html>
---
+++ parts/nav.php
@@ -0,0 +1,23 @@
+<nav class="main-nav">
+  <a href="index.php" class="logo">
+    <img src="assets/images/logo.png" alt="SnapX Photography Template">
+  </a>
+  <ul class="nav">
+    <li><a href="index.php" class="active">Home</a></li>
+    <li class="has-sub">
+      <a href="javascript:void(0)">Photos & Videos</a>
+      <ul class="sub-menu">
+        <li><a href="contests.php">Contests</a></li>
+        <li><a href="contest-details.php">Single Contest</a></li>
+      </ul>
+    </li>
+    <li><a href="categories.php">Categories</a></li>
+    <li><a href="users.php">Users</a></li>
+  </ul>
+  <div class="border-button">
+    <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
+  </div>
+  <a class='menu-trigger'>
+    <span>Menu</span>
+  </a>
+</nav>
