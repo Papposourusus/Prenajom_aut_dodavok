6b61df6077e8be37b87a11b62ebbfdaccd6f2387
Papposourusus <masikpatrik@gmail.com>
2025-03-23 14:07:44 +0100

    oprava

--- parts/footer.php
+++ parts/footer.php
@@ -1,8 +1,10 @@
+
 <footer>
   <div class="container">
     <div class="row">
       <div class="col-lg-12">
-        <p>Copyright © 2048 <a href="#">SnapX</a> Photo Contest Co., Ltd. All rights reserved.
+        <p>Copyright © 2048 <a href="#">SnapX</a> Photo Contest Co., Ltd. All rights reserved. 
+        
         Design: <a title="CSS Templates" rel="sponsored" href="https://templatemo.com/page/1" target="_blank">TemplateMo</a></p>
       </div>
     </div>
--- parts/header.php
+++ parts/header.php
@@ -2,67 +2,8 @@
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <nav class="main-nav">
-          <a href="index.php" class="logo">
-            <img src="assets/images/logo.png" alt="SnapX Photography Template">
-          </a>
-          <ul class="nav">
-            <li><a href="index.php" class="active">Home</a></li>
-            <li class="has-sub">
-              <a href="javascript:void(0)">Photos & Videos</a>
-              <ul class="sub-menu">
-                <li><a href="contests.php">Contests</a></li>
-                <li><a href="contest-details.php">Single Contest</a></li>
-              </ul>
-            </li>
-            <li><a href="categories.php">Categories</a></li>
-            <li><a href="users.php">Users</a></li>
-          </ul>
-          <div class="border-button">
-            <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
-          </div>
-          <a class='menu-trigger'>
-            <span>Menu</span>
-          </a>
-        </nav>
+        <?php include 'parts/nav.php'; ?>
       </div>
     </div>
   </div>
 </header>
-footer.php:
-
-<footer>
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-12">
-        <p>Copyright © 2048 <a href="#">SnapX</a> Photo Contest Co., Ltd. All rights reserved.
-        Design: <a title="CSS Templates" rel="sponsored" href="https://templatemo.com/page/1" target="_blank">TemplateMo</a></p>
-      </div>
-    </div>
-  </div>
-</footer>
-nav.php:
-
-<nav class="main-nav">
-  <a href="index.php" class="logo">
-    <img src="assets/images/logo.png" alt="SnapX Photography Template">
-  </a>
-  <ul class="nav">
-    <li><a href="index.php" class="active">Home</a></li>
-    <li class="has-sub">
-      <a href="javascript:void(0)">Photos & Videos</a>
-      <ul class="sub-menu">
-        <li><a href="contests.php">Contests</a></li>
-        <li><a href="contest-details.php">Single Contest</a></li>
-      </ul>
-    </li>
-    <li><a href="categories.php">Categories</a></li>
-    <li><a href="users.php">Users</a></li>
-  </ul>
-  <div class="border-button">
-    <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
-  </div>
-  <a class='menu-trigger'>
-    <span>Menu</span>
-  </a>
-</nav>
