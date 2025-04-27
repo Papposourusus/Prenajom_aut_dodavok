9589500201c8bdddebd8794c4133b40c66773d96
Papposourusus <masikpatrik@gmail.com>
2025-04-27 18:35:00 +0200

    login?

---
+++ register.php
@@ -0,0 +1,34 @@
+<?php
+$servername = "localhost";
+$username = "root"; 
+$password = "";
+$dbname = "website_comments";  // alebo tvoje meno databázy
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $email = $_POST['email'];
+    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$user', '$email', '$pass')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Registration successful!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
+<form method="POST" action="register.php">
+  <input type="text" name="username" placeholder="Username" required />
+  <input type="email" name="email" placeholder="Email" required />
+  <input type="password" name="password" placeholder="Password" required />
+  <button type="submit">Sign Up</button>
+</form>
