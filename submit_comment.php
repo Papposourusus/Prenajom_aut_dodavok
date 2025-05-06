46b8227423a830d5a9f8aa792e59d67c00dae567
Papposourusus <masikpatrik@gmail.com>
2025-05-06 22:30:23 +0200

    Update submit_comment.php

--- submit_comment.php
+++ submit_comment.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $db = 'website_comments';
 $user = 'root';
-$pass = ''; 
+$pass = '';
 
 $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
@@ -14,12 +14,15 @@ $email = $_POST['email'];
 $message = $_POST['message'];
 
 $stmt = $conn->prepare("INSERT INTO comments (username, email, comment) VALUES (?, ?, ?)");
+if (!$stmt) {
+    die("Chyba v prepare: " . $conn->error);
+}
+
 $stmt->bind_param("sss", $name, $email, $message);
 $stmt->execute();
 $stmt->close();
 $conn->close();
 
-
-header("Location: kontakt.php"); 
+header("Location: kontakt.php"); // Alebo kam chceš presmerovať
 exit;
 ?>
