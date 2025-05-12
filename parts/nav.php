07eb342a869a995f5dd3b95c61b2cd5cdbc9b174
Papposourusus <masikpatrik@gmail.com>
2025-05-12 19:23:15 +0200

    Update nav.php

--- parts/nav.php
+++ parts/nav.php
@@ -1,7 +1,62 @@
 <?php
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 ?>
+
+<nav class="main-nav">
+  <!-- ***** Logo Start ***** -->
+  <a href="index.html" class="logo_d">
+    <img src="assets/images/logo.jpg" alt="Ano ej to AI niet penazi na grafika">
+  </a>
+  <!-- ***** Logo End ***** -->
+  <!-- ***** Menu Start ***** -->
+  <ul class="nav">
+    <li><a href="index.php" class="active">Home</a></li>
+    <li class="has-sub">
+      <a href="javascript:void(0)">zatial nic</a>
+      <ul class="sub-menu">
+        <li><a href="contests.php">Do buducnosti</a></li>
+        <li><a href="contest-details.html">Daco take</a></li>
+      </ul>
+    </li>
+    <li><a href="lokalizacia.php">Naše miesta</a></li>
+    <li><a href="users.php">Users</a></li>
+  </ul>   
+
+  <div class="border-button">
+  <?php if (isset($_SESSION['username'])): ?>
+    <form action="logout.php" method="post" style="display:inline;">
+  <button type="submit" class="logout-button"><i class="fa fa-sign-out-alt"></i> Logout</button>
+</form>
+
+  <?php else: ?>
+    <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
+  <?php endif; ?>
+</div>
+
+
+  </div>
+  <a class='menu-trigger'>
+    <span>Menu</span>
+  </a>
+  <!-- ***** Menu End ***** -->
+</nav>
+
 <style>
+.logo_d {
+  position: relative;
+  left:-5%;
+  width: 16%;
+  height:auto;
+  border-radius: 20%;
+}
+.logo_d img {
+    border-top-right-radius: 20%; /* Aplikuje zaoblenie na obrázok */
+    border-bottom-right-radius: 20%;
+}
+
+
 .logout-button {
   background-color: transparent;
   border: 2px solid #fff;
@@ -18,41 +73,6 @@ session_start();
   background-color: #fff;
   color: #000;
 }
+
+
 </style>
-
-<nav class="main-nav">
-  <!-- ***** Logo Start ***** -->
-  <a href="index.php" class="logo_d">
-    <img src="assets/images/logo.jpg" alt="Ano ej to AI niet penazi na grafika">
-  </a>
-  <!-- ***** Logo End ***** -->
-
-  <!-- ***** Menu Start ***** -->
-  <ul class="nav">
-    <li><a href="index.php" class="active">Home</a></li>
-    <li class="has-sub">
-      <a href="javascript:void(0)">zatial nic</a>
-      <ul class="sub-menu">
-        <li><a href="contests.php">Do buducnosti</a></li>
-        <li><a href="contest-details.html">Daco take</a></li>
-      </ul>
-    </li>
-    <li><a href="lokalizacia.php">Naše miesta</a></li>
-    <li><a href="users.php">Users</a></li>
-  </ul>   
-
-  <div class="border-button">
-    <?php if (isset($_SESSION['username'])): ?>
-      <form action="logout.php" method="post" style="display:inline;">
-        <button type="submit" class="logout-button"><i class="fa fa-sign-out-alt"></i> Logout</button>
-      </form>
-    <?php else: ?>
-      <a id="modal_trigger" href="#modal" class="sign-in-up"><i class="fa fa-user"></i> Sign In/Up</a>
-    <?php endif; ?>
-  </div>
-
-  <a class='menu-trigger'>
-    <span>Menu</span>
-  </a>
-  <!-- ***** Menu End ***** -->
-</nav>
