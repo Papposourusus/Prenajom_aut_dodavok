829b5450317c34f5d453c658d12e7f1f0d156b57
Papposourusus <masikpatrik@gmail.com>
2025-05-14 14:21:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -168,14 +168,14 @@ function showTab(tabId) {
   <!-- ***** Main Banner Area End ***** -->
 
 <?php
-$mysqli = new mysqli("localhost", "root", "", "vehicles"); 
+$mysqli = new mysqli("localhost", "root", "", "auta"); 
 
 if ($mysqli->connect_errno) {
     echo "Failed to connect: " . $mysqli->connect_error;
     exit();
 }
 
-$query = "SELECT * FROM vehicles";
+$query = "SELECT * FROM auta";
 $result = $mysqli->query($query);
 ?>
 
