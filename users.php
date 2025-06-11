749d81a3a1fc0f317f63fdd610095be6b73d55e2
Papposourusus <masikpatrik@gmail.com>
2025-06-11 19:06:11 +0200

    Update users.php

--- users.php
+++ users.php
@@ -12,7 +12,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/comment-form.css">
+    <link rel="stylesheet" href="comment-form.css">
 
     <title>SnapX Photographer Page</title>
 
