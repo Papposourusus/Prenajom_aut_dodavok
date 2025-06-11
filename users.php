74cf56d74be184adebeec994b9f14b9826f42ee2
Papposourusus <masikpatrik@gmail.com>
2025-06-11 19:05:45 +0200

    css

--- users.php
+++ users.php
@@ -12,8 +12,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/comment-form.css">
 
     <title>SnapX Photographer Page</title>
+
+    
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
