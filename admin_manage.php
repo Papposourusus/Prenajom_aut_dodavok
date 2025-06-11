d9d04847debe593b49d475cc54e3f74f84d9313a
Papposourusus <masikpatrik@gmail.com>
2025-06-11 15:18:29 +0200

    db

--- admin_manage.php
+++ admin_manage.php
@@ -1,33 +1,29 @@
 <?php
 session_start();
 if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    exit("Nemáte oprávnenie na zobrazenie tejto stránky.");
+    die("Nemáš oprávnenie pristupovať sem.");
 }
 
 require_once 'db_admin.php';
-require_once 'CarManage\VehicleManager.php';
-require_once 'CarManage\CommentManager.php';
+require_once 'CarManage/VehicleManager.php';
+require_once 'CarManage/CommentManager.php';
 
-// Inicializuj databázy
-$dbAuta = new Database("auta");
-$dbComments = new Database("website_comments");
+// Pripojenie k databázam
+$autaDB = new Database('auta');
+$commentsDB = new Database('website_comments');
 
-// Inicializuj správcov
-$vehicleManager = new VehicleManager($dbAuta->conn);
-$commentManager = new CommentManager($dbComments->conn);
+$vehicleManager = new VehicleManager($autaDB->conn);
+$commentManager = new CommentManager($commentsDB->conn);
 
-// Spracovanie mazania
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (isset($_POST['delete_vehicle'])) {
-        $vehicleManager->deleteVehicle((int)$_POST['vehicle_id']);
-    }
-
-    if (isset($_POST['delete_comment'])) {
-        $commentManager->deleteComment((int)$_POST['comment_id']);
-    }
+// Mazanie záznamov
+if (isset($_POST['delete_vehicle'])) {
+    $vehicleManager->deleteVehicle((int)$_POST['vehicle_id']);
+}
+if (isset($_POST['delete_comment'])) {
+    $commentManager->deleteComment((int)$_POST['comment_id']);
 }
 
-// Získaj dáta
+// Načítanie dát
 $vehicles = $vehicleManager->getAllVehicles();
 $comments = $commentManager->getAllComments();
 ?>
--- db_admin.php
+++ db_admin.php
@@ -3,18 +3,18 @@ class Database {
     private $host = "localhost";
     private $user = "root";
     private $pass = "";
-    private $db_name;
+    private $dbname;
     public $conn;
 
-    public function __construct($db_name) {
-        $this->db_name = $db_name;
+    public function __construct($dbname) {
+        $this->dbname = $dbname;
         $this->connect();
     }
 
-    public function connect() {
-        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db_name);
+    private function connect() {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
         if ($this->conn->connect_error) {
-            die("Chyba pripojenia k DB (" . $this->db_name . "): " . $this->conn->connect_error);
+            die("Chyba pripojenia k DB ($this->dbname): " . $this->conn->connect_error);
         }
     }
 }
