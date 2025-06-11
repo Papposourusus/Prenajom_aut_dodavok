dfab1bac8dcc92c5816cf3885b5051e23e550147
Papposourusus <masikpatrik@gmail.com>
2025-06-11 12:06:00 +0200

    f

--- Database.php
+++ Database.php
@@ -1,14 +1,18 @@
 <?php
 class Database {
-    
-$mysqli = new mysqli("localhost", "root", "", "auta"); 
-if ($mysqli->connect_errno) {
- echo "Failed to connect: " . $mysqli->connect_error;
- exit();
-}
-$query = "SELECT * FROM auta";
-$result = $mysqli->query($query);
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $dbname = "auta";
+    public $conn;
 
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->dbname);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
 }
 
 ?>
