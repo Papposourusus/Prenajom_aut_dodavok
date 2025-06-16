5f5bd2fcc48e479a81dfcdbdf59f3d4d01e90ba8
Papposourusus <masikpatrik@gmail.com>
2025-06-16 17:47:45 +0200

    Update submit_comment.php

--- submit_comment.php
+++ submit_comment.php
@@ -1,16 +1,14 @@
 <?php
-require_once 'database.php'; // súbor, kde je tvoja Database trieda
+require_once 'database.php'; 
 require_once 'lokalizacia/CommentRepository.php';
 require_once 'Comment.php';
 
 // vytvorenie inštancie pripojenia
 $dbInstance = new Database();
-$conn = $db->connect();
-; // musíš mať takú metódu, ktorá vráti mysqli alebo PDO
+$conn = $dbInstance->connect();  // tu použi $dbInstance, nie $db
 
 $commentRepo = new CommentRepository($conn);
 
-// použitie Comment objektu podľa post dát
 $comment = new Comment([
     'user' => $_POST['name'],
     'comment' => $_POST['message'],
@@ -23,4 +21,3 @@ if ($commentRepo->addComment($comment)) {
     echo "Nastala chyba pri ukladaní komentára.";
 }
 ?>
-
