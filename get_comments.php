91599d7874cc35fc3006ad0c83e2076faf6a832c
Papposourusus <masikpatrik@gmail.com>
2025-06-16 16:47:42 +0200

    Update get_comments.php

--- get_comments.php
+++ get_comments.php
@@ -7,7 +7,7 @@ class Comment {
 
     public function __construct(array $data) {
         $this->id = $data['id'] ?? null;
-        $this->username = $data['username'];
+        $this->username = $data['user_id'];
         $this->comment = $data['comment'];
         $this->created_at = $data['created_at'] ?? date('Y-m-d H:i:s');
     }
