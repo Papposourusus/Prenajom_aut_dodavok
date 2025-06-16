f32699357bfb600e599d7c124e5c59f69e1b1f10
Papposourusus <masikpatrik@gmail.com>
2025-06-16 17:57:44 +0200

    Update get_comments.php

--- lokalizacia/get_comments.php
+++ lokalizacia/get_comments.php
@@ -1,14 +1,14 @@
 <?php
 require_once 'Comment.php';
-require_once 'lokalizacia\CommentRepository.php.php';
+require_once 'lokalizacia/CommentRepository.php';  // opravená cesta
 require_once 'Database.php';
 
 header('Content-Type: application/json');
 
 $db = new Database();
-$conn = $db->connect();
+$conn = $db->connect();  // mysqli spojenie
 
-$commentRepo = new CommentRepository($conn);
+$commentRepo = new CommentRepository($conn);  // musí pracovať s mysqli
 $comments = $commentRepo->getAllComments();
 
 echo json_encode($comments);
